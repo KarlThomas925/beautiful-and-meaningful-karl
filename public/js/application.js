1d795a1f9b78d645537acea137b546714557f3f1
Karl Thomas <email": null>
2017-03-23 22:38:02 -0500

    implement google maps api and it's nearby search function

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,69 @@
 $(document).ready(function () {
 
 });
+var map;
+var infoWindow;
+var currentPos;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow({map: map});
+  var service = new google.maps.places.PlacesService(map);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      currentPos = {
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude
+          };
+      map.setCenter(currentPos);
 
+      var posMarker = new google.maps.Marker({
+        map: map,
+        position: currentPos,
+        animation: google.maps.Animation.DROP
+      });
 
-
-
-
-function formScrape (formObject){
-  if (formObject.find('input').attr('name') === "_method"){
-    var realMethod = formObject.find('input').attr('value');
+      service.nearbySearch({
+        location: currentPos,
+        radius: 50000,
+        type: ['campground']
+      }, callback);
+    }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+    });
   } else {
-    var realMethod = formObject.attr('method');
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
-  var scrape = {
-    url: formObject.attr('action'),
-    method: realMethod,
-    data: formObject.serialize()
-  };
-  return scrape
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
